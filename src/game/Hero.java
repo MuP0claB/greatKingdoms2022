973a8aa2d214faaf5924f46a5dd47d77702096d5
Olegov <vladimir.olegov.krystev@gmail.com>
2023-03-13 21:37:05 +0200

    Adding ability.

--- src/game/Hero.java
+++ src/game/Hero.java
@@ -1,13 +1,16 @@
 package game;
 
 public enum Hero {
-   MOUNTAIN_HERO_1("Marko The Great", 110),
-   MOUNTAIN_HERO_2("Jeana D'Arc", 95);
+   MOUNTAIN_HERO_1("Marko The Great", 110, SuperAbility.HYPER_VENTILATION),
+   MOUNTAIN_HERO_2("Jeana D'Arc", 95, SuperAbility.HYPER_VENTILATION);
     public final String name;
     public final int healthPoints;
 
-    private Hero(String name, int healthPoints) {
+    public final SuperAbility ability;
+
+    private Hero(String name, int healthPoints, SuperAbility ability) {
         this.name = name;
         this.healthPoints = healthPoints;
+        this.ability = ability;
     }
 }
