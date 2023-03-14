28cba8676393bd9e5cbb1af955ac195c460d2605
Kaloyan Ivanov <kaloyan.ivanov35@gmail.com>
2023-03-14 22:18:45 +0200

    Added PrincessName and Location

---
+++ src/game/Location.java
@@ -0,0 +1,5 @@
+package game;
+
+public enum Location {
+    THE_MOUNTAIN_PARADISE, LA_AREA_EMPIRE, THE_FLOATING_ISLAND;
+}
---
+++ src/game/PrincessName.java
@@ -0,0 +1,5 @@
+package game;
+
+public enum PrincessName {
+    VICTORIA, MELISSA, LOLA_FERRARI;
+}
