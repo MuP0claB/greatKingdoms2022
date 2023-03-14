6bc664c98362c1177432f00856d460373158d5c0
MuP0claB <miroslav.ivanoff@gmail.com>
2023-03-14 22:41:01 +0200

    Adding LOLLA FERRARI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import game.GameLocationSettings;
-import game.GameStoryLocation;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println();
