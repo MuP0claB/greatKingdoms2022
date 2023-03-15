6ace217cbf02785c8447f6fd06d99037b128c4c5
stank <stankov1505@gmail.com>
2023-03-15 21:35:46 +0200

    creating pseodo code

--- src/game/Game.java
+++ src/game/Game.java
@@ -6,18 +6,19 @@ public class Game {
 
     public static void main(String[] args) {
 
-       chooseLocation();
+        chooseLocation();
 
     }
 
-    public static void printMenu(){
+    public static void printMenu() {
         System.out.println("1. Go to shop");
         System.out.println("2. Check inventory");
         System.out.println("3. Take resources");
         System.out.println("4. Increase your skills");
         System.out.println("5. Take your journey");
     }
-    public static void chooseLocation(){
+
+    public static void chooseLocation() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Choose your location.");
         System.out.println("1. Sea");
@@ -25,26 +26,9 @@ public class Game {
         System.out.println("3. Desert");
 
         int choose = scanner.nextInt();
-
-        switch (choose){
-            case 1:
-                Hero.printHeroInfo();
-               int heroChoose = scanner.nextInt();
-               switch (heroChoose){
-                   case 1:
-                    Hero hero = Hero.MOUNTAIN_HERO_MALE;
-
-                       break;
-                   case 2:
-                       break;
-               }
-                break;
-            case 2:
-                System.out.println("sdds");
-                break;
-            case 3:
-                System.out.println("dsads");
-                break;
-        }
+// Избира се location, като се избере локация след това се избира герой.
+        След,като се избере герой, той се запазва в даден обект.Запазва се и си остава до края на играта с него.
     }
+
+
 }
