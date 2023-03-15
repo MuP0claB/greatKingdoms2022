04f538d78a5e3ce24f3755fb9c293b8002bb19e0
Kaloyan Ivanov <kaloyan.ivanov35@gmail.com>
2023-03-15 21:00:36 +0200

    Added printHeroInfo method.

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,8 +1,50 @@
 package game;
 
+import java.util.Scanner;
+
 public class Game {
 
     public static void main(String[] args) {
-        System.out.println("Hello from BG");
+
+       chooseLocation();
+
+    }
+
+    public static void printMenu(){
+        System.out.println("1. Go to shop");
+        System.out.println("2. Check inventory");
+        System.out.println("3. Take resources");
+        System.out.println("4. Increase your skills");
+        System.out.println("5. Take your journey");
+    }
+    public static void chooseLocation(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose your location.");
+        System.out.println("1. Sea");
+        System.out.println("2. Mountain");
+        System.out.println("3. Desert");
+
+        int choose = scanner.nextInt();
+
+        switch (choose){
+            case 1:
+                Hero.printHeroInfo();
+               int heroChoose = scanner.nextInt();
+               switch (heroChoose){
+                   case 1:
+                    Hero hero = Hero.MOUNTAIN_HERO_MALE;
+
+                       break;
+                   case 2:
+                       break;
+               }
+                break;
+            case 2:
+                System.out.println("sdds");
+                break;
+            case 3:
+                System.out.println("dsads");
+                break;
+        }
     }
 }
--- src/game/Hero.java
+++ src/game/Hero.java
@@ -1,16 +1,24 @@
 package game;
 
 public enum Hero {
-   MOUNTAIN_HERO_1("Marko The Great", 110, SuperAbility.HYPER_VENTILATION),
-   MOUNTAIN_HERO_2("Jeana D'Arc", 95, SuperAbility.HYPER_VENTILATION);
+
+    MOUNTAIN_HERO_MALE("Marko The Great", 110, SuperAbility.HYPER_VENTILATION),
+    MOUNTAIN_HERO_FEMALE("Jeana D'Arc", 95, SuperAbility.HYPER_VENTILATION);
     public final String name;
     public final int healthPoints;
 
     public final SuperAbility ability;
 
+
     private Hero(String name, int healthPoints, SuperAbility ability) {
         this.name = name;
         this.healthPoints = healthPoints;
         this.ability = ability;
+    }
+
+    public static void printHeroInfo() {
+        System.out.println(MOUNTAIN_HERO_MALE.name + ", has " + MOUNTAIN_HERO_MALE.healthPoints + " HP, and his super ability is " + MOUNTAIN_HERO_MALE.ability + ".");
+        System.out.println(MOUNTAIN_HERO_FEMALE.name + ", has " + MOUNTAIN_HERO_FEMALE.healthPoints + " HP, and her super ability is " + MOUNTAIN_HERO_FEMALE.ability + ".");
+
     }
 }
