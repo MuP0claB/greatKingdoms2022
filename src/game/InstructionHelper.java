b3614ffb3f4ef58e8cc64410ce3152823a969b9a
Kaloyan Ivanov <kaloyan.ivanov35@gmail.com>
2023-06-05 20:43:13 +0300

    fix Potion info

--- src/game/InstructionHelper.java
+++ src/game/InstructionHelper.java
@@ -22,10 +22,10 @@ public class InstructionHelper {
 
     public static void printShopEquipment() {
         System.out.println("1. Armour 🛡 COST: 50 coins. Increase defense: 20 points ");
-        System.out.println("2. Shield 🛡️ - 40 coins. Increase defense: 10 points "); // increase hero hp with
+        System.out.println("2. Shield 🛡️ - 40 coins. Increase defense: 10 points ");
         System.out.println("3. Knife ⚔🗡️ - 40 coins. Increase attack: 15 points ");
         System.out.println("4. Gun 🔫- 50 coins. Increase attack: 25 points ");
-        System.out.println("5. Potion - 50 coins."); // increase hero hp with 20%
+        System.out.println("5. Potion - 50 coins. Increase HP with up to 20 ");
         System.out.println("6. Back. ");
         System.out.println("Please select between 1 & 6 to choose your equipment - shop ");
         System.out.println("\033[31m< - > < - > < - > < - >\033[0m");
